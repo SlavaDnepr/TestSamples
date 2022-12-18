5ca2d01668beadc7b1664c3fb6796b772c4bc982
Viacheslav Malyi <viacheslavm@leadsmarket.com>
2022-12-18 15:24:25 +0200

    no message

--- SortedDetector/src/Main.java
+++ SortedDetector/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int intArray[] = new int[]{ 1,2,3,4,5 };
+        boolean isSorted = Main.isSorted(intArray, SortOrder.ASC);
+        System.out.println("isSorted: " + isSorted);
+    }
+
+    public static boolean isSorted(int[] array, SortOrder sortOrder)
+    {
+        if (array.length <= 1)
+            return true;
+
+        for (int i = 1; i < array.length; i++) {
+            if (sortOrder == SortOrder.ASC)
+                if (array[i] < array[i - 1])
+                    return false;
+
+            if (sortOrder == SortOrder.DESC)
+                if (array[i] > array[i - 1])
+                    return false;
+        }
+
+        return true;
     }
 }
---
+++ SortedDetector/src/MainTest.java
@@ -0,0 +1,82 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void isSortedPositive1() {
+        int intArray[] = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.ASC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedPositive2() {
+        int intArray[] = new int[]{ 10,9,8,7,6,5,4,3,2,1 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.DESC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedPositive3() {
+        int intArray[] = new int[]{ 1,1,1,1,1 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.ASC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedPositive4() {
+        int intArray[] = new int[]{ 1,1,1,1,1 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.DESC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedPositive5() {
+        int intArray[] = new int[]{ };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.DESC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedPositive6() {
+        int intArray[] = new int[]{ -5, 0, 0, 5};
+        boolean actualResult = Main.isSorted(intArray, SortOrder.ASC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedPositive7() {
+        int intArray[] = new int[]{ 5, 0, 0, -5};
+        boolean actualResult = Main.isSorted(intArray, SortOrder.DESC);
+        assertEquals(actualResult, true);
+    }
+
+    @Test
+    void isSortedNegative1() {
+        int intArray[] = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.DESC);
+        assertEquals(actualResult, false);
+    }
+
+    @Test
+    void isSortedNegative2() {
+        int intArray[] = new int[]{ 10,9,8,7,6,5,4,3,2,1 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.ASC);
+        assertEquals(actualResult, false);
+    }
+
+    @Test
+    void isSortedNegative3() {
+        int intArray[] = new int[]{ 1,7,8,0 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.DESC);
+        assertEquals(actualResult, false);
+    }
+
+    @Test
+    void isSortedNegative4() {
+        int intArray[] = new int[]{ 1,7,8,0 };
+        boolean actualResult = Main.isSorted(intArray, SortOrder.ASC);
+        assertEquals(actualResult, false);
+    }
+}
