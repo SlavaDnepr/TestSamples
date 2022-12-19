2638c31e9ced0d67f250e16217284dc162ed117b
Viacheslav Malyi <viacheslavm@leadsmarket.com>
2022-12-19 11:15:31 +0200

    FunctionsTask3

---
+++ FunctionsTask3/src/FunctionsTask3.java
@@ -0,0 +1,19 @@
+public class FunctionsTask3 {
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     * if set invalid arguments in method, then method must throws
+     * IllegalArgumentException
+     */
+    public static int multiArithmeticElements(int a1, int t, int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException();
+
+        int multiArithmeticElements = 1;
+        for (int i = 0; i < n; i++)
+            multiArithmeticElements *= (a1+t*i);
+
+        return multiArithmeticElements;
+    }
+}
